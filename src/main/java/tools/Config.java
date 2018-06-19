f360f6d1b56839017d249bed3072d28d0df543c2
Lucas <lucas_kneis@yahoo.de>
2018-06-19 19:32:04 +0200

    MONGODB

--- src/main/java/tools/Config.java
+++ src/main/java/tools/Config.java
@@ -6,7 +6,7 @@ import javax.persistence.GenerationType;
 public class Config {
 
    // public static final Integer NUMBER_OF_CUSTOMERS = 100;
-    public static final String PERSISTENCE_UNIT_NAME = PERSISTENCE_UNITS.MYSQL.name();
+    public static final String PERSISTENCE_UNIT_NAME = PERSISTENCE_UNITS.OGM_MONGODB.name();
 
     public enum PERSISTENCE_UNITS {
         MYSQL, OGM_MONGODB, OGM_NEO4J, OGM_CASSANDRA, OGM_REDIS,
